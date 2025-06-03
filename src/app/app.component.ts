fd402eded25e89130795d291465f874eddc8dfa6
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-03 11:53:51 +0000

    Add header component and placeholder buttons and filter buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
