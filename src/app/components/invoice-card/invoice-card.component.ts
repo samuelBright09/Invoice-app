80aca6d56dd234de7627a7224385fdd6ab92bf26
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-03 14:30:00 +0000

    Add invoice-card component

---
+++ src/app/components/invoice-card/invoice-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-card',
+  imports: [],
+  templateUrl: './invoice-card.component.html',
+  styleUrl: './invoice-card.component.scss'
+})
+export class InvoiceCardComponent {
+
+}
