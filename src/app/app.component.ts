168f21b3a377f1ca1cff4f1e2700b4f00af41b2e
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-03 14:30:00 +0000

    Add invoice-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, InvoiceCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
