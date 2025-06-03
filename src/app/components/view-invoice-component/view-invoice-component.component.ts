c618c4f65652e9a4201a6b8a4083f5095b003015
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-03 16:04:56 +0000

    Add view invoice page

---
+++ src/app/components/view-invoice-component/view-invoice-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-invoice-component',
+  imports: [],
+  templateUrl: './view-invoice-component.component.html',
+  styleUrl: './view-invoice-component.component.scss'
+})
+export class ViewInvoiceComponentComponent {
+
+}
