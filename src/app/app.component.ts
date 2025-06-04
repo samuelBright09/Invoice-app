dc5558c175eaffb7003b94ed7e0da71b99d89fb5
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-04 17:27:07 +0000

    Add status header to invoice details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { EmptyInvoiceComponent } from "./components/empty-invoice/empty-invoice.component";
+// import { EmptyInvoiceComponent } from "./components/empty-invoice/empty-invoice.component";
 // import { DeleteModalComponentComponent } from "./components/delete-modal-component/delete-modal-component.component";
 // import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
-// import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
+import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
 // import { NewInvoiceFormComponent } from "./components/new-invoice-form/new-invoice-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, EmptyInvoiceComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, ViewInvoiceComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
