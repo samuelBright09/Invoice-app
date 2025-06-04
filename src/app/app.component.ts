283a646c118ff0614ed846326b2ec3dc09041b6d
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-04 16:29:35 +0000

    Add delete approval modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 import { EmptyInvoiceComponent } from "./components/empty-invoice/empty-invoice.component";
+// import { DeleteModalComponentComponent } from "./components/delete-modal-component/delete-modal-component.component";
 // import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
 // import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
 // import { NewInvoiceFormComponent } from "./components/new-invoice-form/new-invoice-form.component";
---
+++ src/app/components/delete-modal-component/delete-modal-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-modal-component',
+  imports: [],
+  templateUrl: './delete-modal-component.component.html',
+  styleUrl: './delete-modal-component.component.scss'
+})
+export class DeleteModalComponentComponent {
+
+}
