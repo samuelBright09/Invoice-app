9c8c261bd0b8314aed89740d8a89a7c493ba8cec
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-04 17:10:53 +0000

    Add filter button and dropdown feature.

--- src/app/components/header-component/header-component.component.ts
+++ src/app/components/header-component/header-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FilterButtonComponentComponent } from "../filter-button-component/filter-button-component.component";
 // import { ButtonComponentComponent } from "../button-component/button-component.component";
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [FilterButtonComponentComponent],
   templateUrl: './header-component.component.html',
   styleUrl: './header-component.component.scss'
 })
