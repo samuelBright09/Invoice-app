5336f3d45d05b7c243b484de825d9af78f2625fa
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-04 11:07:37 +0000

    Add empty invoice UI component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { EmptyInvoiceComponent } from "./components/empty-invoice/empty-invoice.component";
 // import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
-import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
-import { NewInvoiceFormComponent } from "./components/new-invoice-form/new-invoice-form.component";
+// import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
+// import { NewInvoiceFormComponent } from "./components/new-invoice-form/new-invoice-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, ViewInvoiceComponentComponent, NewInvoiceFormComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, EmptyInvoiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/empty-invoice/empty-invoice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-invoice',
+  imports: [],
+  templateUrl: './empty-invoice.component.html',
+  styleUrl: './empty-invoice.component.scss'
+})
+export class EmptyInvoiceComponent {
+
+}
