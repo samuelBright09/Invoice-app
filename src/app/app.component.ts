8279f7b3f7048960bdda4920849f69ed30d378d5
samuelBright09 <samuelbrightsk@gmail.com>
2025-06-04 10:44:26 +0000

    Add form componet and designed it as a modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 // import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
 import { ViewInvoiceComponentComponent } from "./components/view-invoice-component/view-invoice-component.component";
+import { NewInvoiceFormComponent } from "./components/new-invoice-form/new-invoice-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent,  ViewInvoiceComponentComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponentComponent, ViewInvoiceComponentComponent, NewInvoiceFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
